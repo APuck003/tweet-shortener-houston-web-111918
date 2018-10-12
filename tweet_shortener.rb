f2cd04b21bf8351b5565d0dfa4ba0f57e095ba59
Austin Puckett <puckett_austin@yahoo.com>
2018-10-12 19:19:57 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -11,5 +11,29 @@ def dictionary
   'and' => '&'}
 end
 
-def word_substituter(string)
-  
+def word_substituter(tweet)
+  words = tweet.split(" ")
+  short_tweet = []
+
+  words.each do |word|
+    case word.downcase
+    when "hello"
+      short_tweet << "hi"
+    when "to", "two", "too"
+      short_tweet << "2"
+    when "for", "four" 
+      short_tweet << "4"
+    when "be" 
+      short_tweet << "b"
+    when "you" 
+      short_tweet << "u"
+    when "at" 
+      short_tweet << "@" 
+    when "and" 
+      short_tweet << "&"
+    else
+      short_tweet << word
+    end
+  end
+  short_tweet.join(" ")
+end
